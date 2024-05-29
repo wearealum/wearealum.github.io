b70f980c908931b19a1094d5167f44e74cccbbe5
wearealum <shyamal.southeastchemicals@gmail.com>
2024-05-29 12:54:25 +0530

    Update script1.js

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,14 +1,16 @@
+var footer = document.querySelector('footer');
+var contentHeight = document.body.scrollHeight; // Height of the entire document
+var footerVisible = false;
+
 window.addEventListener('scroll', function() {
-    var footer = document.querySelector('footer');
-    var contentHeight = document.body.scrollHeight; // Height of the entire document
     var scrollPosition = window.innerHeight + window.scrollY; // Current scroll position
 
-    // If scrolled to the bottom of the page
-    if (scrollPosition >= contentHeight) {
-        if (footer.style.display !== 'block') {
-            footer.style.display = 'block'; // Show the footer only if it's not already visible
-        }
-    } else {
-        footer.style.display = 'none'; // Hide the footer if not scrolled to the bottom
+    // If scrolled to the bottom of the page and footer is not already visible
+    if (scrollPosition >= contentHeight && !footerVisible) {
+        footer.style.display = 'block'; // Show the footer
+        footerVisible = true; // Update flag to indicate footer is visible
+    } else if (scrollPosition < contentHeight && footerVisible) {
+        footer.style.display = 'none'; // Hide the footer
+        footerVisible = false; // Update flag to indicate footer is not visible
     }
 });
